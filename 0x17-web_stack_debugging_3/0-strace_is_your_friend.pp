54d85f219547125c2bd4c2f6adcd75aa5c595d55
ElmahdiAI <mehdirajawi239@gmail.com>
2024-08-15 04:32:55 -0700

    typo fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
-# Fixes Apache 500 error
+# Fixes Apache 500 error by correcting the 'phpp' typo in wp-settings.php if the file exists
 
 exec { 'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/bin:/usr/local/bin',
+  onlyif  => 'test -f /var/www/html/wp-settings.php',
 }
