7d566296442cc6bb20390833186965fd7d9944e3
ElmahdiAI <mehdirajawi239@gmail.com>
2024-08-15 03:37:57 -0700

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Fixes Apache 500 error
+# Fixes error "phpp
 
-exec { 'fix-wordpress':
+exec{'fix-wordpress':
 	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path    => '/usr/local/bin/:/bin/'
+	path => '/usr/local/bin/:/bin/'
 }
